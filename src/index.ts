024940835a97472d12e956280824e62dd57d7e0f
shieldx <anhnv.24810310060@epu.edu.vn>
2025-12-23 19:20:00 +0700

    refactor: format code and update response message in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-    // src/index.ts
-    import express, { Request, Response } from 'express';
+// src/index.ts
+import express, { Request, Response } from 'express';
 
-    const app = express();
-    const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-    app.get('/', (req: Request, res: Response) => {
-      res.send('Hello from Express with TypeScript!');
-    });
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Express with TypeScript - V1 !');
+});
 
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port} - V1`);
+});
