c0918eac6e55660fd087694edcc1e664c8ff1432
shieldx <anhnv.24810310060@epu.edu.vn>
2025-12-23 19:29:30 +0700

    fix: update version in response message and console log in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express with TypeScript - V2 !');
+  res.send('Hello from Express with TypeScript - V3 !');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port} - V2`);
+  console.log(`Server is running on http://localhost:${port} - V3`);
 });
